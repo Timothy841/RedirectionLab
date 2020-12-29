3ea904ba5911842f094755c212c31fc959ecbccc
Timothey <>
2020-12-29 18:30:16 -0500

    Made the class able to take in files and output stars.

---
+++ MakeStars.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class MakeStars{
+  public static void main( String[]args ){
+      //use the standard input (terminal input)
+      //as the string that you read from
+      Scanner n = new Scanner( System.in );
+      String c = "";
+      while(n.hasNextLine()){
+        Scanner a = new Scanner(n.nextLine());
+        while (a.hasNext()){
+          String b = a.next();
+          for (int i = 0; i<b.length();i++){
+            if (b.charAt(i)!=' '){
+              c+="*";
+            }
+          }
+          c+=" ";
+        }
+        if (n.hasNextLine()){
+          c+="\n";
+        }
+      }
+      System.out.println(c);
+      //use hasNextLine()/nextLine() to loop over
+      //all of the data
+
+      //If you want to read the input word by word
+      //this can be replaced with hasNext() and next()
+
+  }
+}
